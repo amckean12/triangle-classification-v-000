98523af68c5f641b9005f604cfb0e71d936e6a2a
Alex McKean <amckean@me.com>
2017-12-13 02:24:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
     end
     if((@side_1 + @side_2 < @side_3) || (@side_1 + @side_3 < @side_2) || (@side_2 + @side_3 < @side_1))
     raise TriangleError
+  end 
   end
     if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
