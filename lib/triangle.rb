d47eece539dfa2ee806efe7e296f44c7dedfac47
Alex McKean <amckean@me.com>
2017-12-13 02:16:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       :isosceles
     elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
       :scalene
-    elsif @side_1 == nil && @side_2 == nil && @side_3 == 0
+    elsif @side_1 == nil && @side_2 == nil && @side_3 == nil
       raise TriangleError
     end
   end
