97d1a27ed1360f3395f8f886c6b4f913d97fbf5a
Alex McKean <amckean@me.com>
2017-12-13 02:03:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,5 +8,10 @@ class Triangle
   end
 
   def kind
+    if @side_1 = @side_2 && @side_1 = @side_3
+      "equilateral"
+    elsif @side_1 = @side_2 || @side_1 = @side_3 
+      "isosceles"
+    end 
   end 
 end
